928ff0a5c50f50d174d4aaf86132cd5206dd1da8
yosoof3 <boxicalcube@proton.me>
2024-05-18 11:10:31 -0400

    fix: :pushpin: set default browser

--- homes/x86_64-linux/yusof@bigbox/mime.nix
+++ homes/x86_64-linux/yusof@bigbox/mime.nix
@@ -1,8 +1,16 @@
+let
+  browser = "firefox.desktop";
+in
 {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
       "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "text/html" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "x-scheme-handler/about" = browser;
     };
   };
 }
+
